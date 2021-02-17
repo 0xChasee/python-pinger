7d453636b23a2823c14b91c5585730d41b57c29c
Chase <79139328+0xChasee@users.noreply.github.com>
2021-02-17 09:01:12 +0100

    Add files via upload

---
+++ python-pinger.py
@@ -0,0 +1,16 @@
+import os
+
+print('#' * 60)
+
+print('python-pinger by 0xChasee') 
+
+print('#' * 60)
+print('-' * 60)      
+
+ip_to_check = input('Please put your IP to ping: ')
+
+print('-' * 60)
+os.system('ping -n 4 {}'.format(ip_to_check))
+print('-' * 60)
+
+input('Press Enter to exit.')
